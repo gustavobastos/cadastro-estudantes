8003d79f27d1a3d10aeba6bb9c42b779f2b17d1e
gustavobastos <gustavosoad@gmail.com>
2019-05-03 17:27:04 -0300

    adição de cabeçalho e rodapé

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -2,20 +2,41 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>CadastroEstudantes</title>
+  <title>Cadastro de Estudantes</title>
   <base href="/">
+  <!-- As a link -->
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 <body>
+  <header>
+    <div class="row bg-primary">
+      <h1 class="col text-white-50">Escola Municipal do Bom Menino</h1>
+    </div>
+    <div class="row imagens">
+      <img class ="col-4" src ="img/1.png">
+      <img class ="col-4" src ="img/2.png">
+      <img class ="col-4" src ="img/3.png">
+    </div>
+  </header>
+<br>
+<hr>
   <ce-root></ce-root>
+<hr>
+<br>
+  <footer>
+  <div class="row bottom rodape">
+    <span class="col-1">V 1.0</span>
+    <span class="col-7"></span>
+    <span class="col-4 text-right">Autor: Gustavo Bastos</span>
+  </footer>
+</header>
 <script type="text/javascript" src="js/runtime.js"></script>
 <script type="text/javascript" src="js/es2015-polyfills.js" nomodule></script>
 <script type="text/javascript" src="js/polyfills.js"></script>
 <script type="text/javascript" src="js/styles.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
 <script type="text/javascript" src="js/vendor.js"></script>
-<script type="text/javascript" src="js/main.js"></script>
-
-</body>
+<script type="text/javascript" src="js/main.js"></script></body>
 </html>
