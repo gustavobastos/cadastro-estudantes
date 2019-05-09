91d53f5e849e1c3acafb34614e3dbdca6bfd6225
gustavobastos <gustavosoad@gmail.com>
2019-05-09 11:18:58 -0300

    Script da base de dados utilizada no desenvolvimento diponibilizado

---
+++ public/DataBase/cadastro_estudantes.sql
@@ -0,0 +1,120 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 09-Maio-2019 às 10:58
+-- Versão do servidor: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cadastro_estudantes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `enderecos`
+--
+
+CREATE TABLE `enderecos` (
+  `estudante` int(11) NOT NULL,
+  `cep` varchar(8) NOT NULL,
+  `rua` varchar(120) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `complemento` varchar(50) DEFAULT NULL,
+  `bairro` varchar(100) NOT NULL,
+  `cidade` varchar(40) NOT NULL,
+  `estado` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estudantes`
+--
+
+CREATE TABLE `estudantes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `nascimento` date NOT NULL,
+  `serie_ingresso` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `maes`
+--
+
+CREATE TABLE `maes` (
+  `nome` varchar(100) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `data_pagamento` date NOT NULL,
+  `estudante` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `enderecos`
+--
+ALTER TABLE `enderecos`
+  ADD PRIMARY KEY (`estudante`);
+
+--
+-- Indexes for table `estudantes`
+--
+ALTER TABLE `estudantes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `maes`
+--
+ALTER TABLE `maes`
+  ADD PRIMARY KEY (`cpf`),
+  ADD KEY `estudante` (`estudante`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `estudantes`
+--
+ALTER TABLE `estudantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `enderecos`
+--
+ALTER TABLE `enderecos`
+  ADD CONSTRAINT `enderecos_ibfk_1` FOREIGN KEY (`estudante`) REFERENCES `estudantes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `maes`
+--
+ALTER TABLE `maes`
+  ADD CONSTRAINT `maes_ibfk_1` FOREIGN KEY (`estudante`) REFERENCES `estudantes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
